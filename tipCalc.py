d960d2896f8705c14948ed2ede3e56cf32404085
derekbeck02 <derekbeck02@gmail.com>
2025-05-15 11:01:03 -0400

    Added coin.png, tipCalc.py

---
+++ tipCalc.py
@@ -0,0 +1,39 @@
+# tipCalc.py
+# Simple tip calculator with tkinter GUI
+# By: Derek Beck
+
+import tkinter as tk
+import ttkbootstrap as tb
+from tkinter import ttk
+
+def main():
+
+    # Variables Init
+    totalBefore = tk.StringVar()
+    
+    # Window creation
+    window = tb.Window(
+        themename = "superhero",
+        title = "Tip Calculator",
+        iconphoto = "coin.png")
+    window.geometry("500x600")
+
+    # Input label
+    inputLabel = ttk.Label(
+        text = "Total Cost Before Tip : ",
+        font = "MV_Boli 25 bold"
+    )
+    inputLabel.pack()
+
+    # Total Before Tip Input Box
+    inputBox = ttk.Entry(
+        window,
+        textvariable = totalBefore,
+    )
+    
+    # Run
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
